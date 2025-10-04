2f82ea65480779c59256952e91101168141144dc
josdic1 <joshuadicker@gmail.com>
2025-10-04 15:36:46 -0400

    update gh repo

---
+++ backend/app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/api/test')
+def test_db():
+    try:
+        db.session.execute('SELECT 1')
+        return "DB Connection Success!"
+    except Exception as e:
+        return f"DB Connection Failed: {e}"
+    
+# routes #
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "This data came from the Flask backend!"})
+    
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
