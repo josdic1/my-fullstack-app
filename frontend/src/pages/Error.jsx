ad348860fcbe83a4a2199eb5c48d9cdd9d310a35
josdic1 <joshuadicker@gmail.com>
2025-10-04 16:30:47 -0400

    Fix import syntax in Error.jsx

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,24 +1,17 @@
-import useRouterError from "react-router-dom";
-import NavBar from "../components/NavBar";
+import { useRouteError } from "react-router-dom";
 
-function ErrorPage() {
+export default function ErrorPage() {
 
-    const error = useRouterError();
-    const status = error.status || 500;
-    const message = error.statusText || error.message || "Something went wrong.";
+  const error = useRouteError();
+  console.error(error);
 
-    console.error(error);
-
-return (
-<>
-<header>
-    <NavBar />
-</header>
-<main>
-    <h1>{status}</h1>
-    <p>{message}</p>  
-</main>
-</>
-)}
-
-export default ErrorPage
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
